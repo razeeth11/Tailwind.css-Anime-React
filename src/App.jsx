e6b0f741eb8d7f975a0ff934797fc2f2b7929756
razeeth11 <razeeth011@gmail.com>
2023-07-24 18:33:24 +0530

    Tailwind.CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import { AppBar } from "./AppBar";
 
 function App() {
-
   return (
     <>
-
+      <AppBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/AppBar.jsx
@@ -0,0 +1,22 @@
+export function AppBar() {
+  return (
+    <div className="app-bar">
+      <div>
+        <h1>Anime <span>World</span></h1>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="#">Naruto</a>
+          </li>
+          <li>
+            <a href="#">One Piece</a>
+          </li>
+          <li>
+            <a href="#">Bleach</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
